e81e37522c88642034c701ca171325714891eaba
Mario Lopez <mariolopjr@users.noreply.github.com>
2025-11-02 09:18:24 -0500

    remove treefmt, not working at the moment

--- modules/flake/treefmt.nix
+++
@@ -1,9 +0,0 @@
-{inputs, ...}: {
-  flake-file.inputs.treefmt-nix = {
-    url = "github:numtide/treefmt-nix";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-  imports = [
-    inputs.flake-file.flakeModules.treefmt-nix
-  ];
-}
